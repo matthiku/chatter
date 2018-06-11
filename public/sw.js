158a7a389a4afb56271d6f6cded4255bea340287
Matthias Kuhs <matthiku@yahoo.com>
2018-06-11 15:25:20 +0100

    improving caching strategies

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,7 @@
 importScripts("precache-manifest.98676f90810c92abe2f2c23362d9e80a.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
+/*global workbox*/
+/*eslint no-undef: "error"*/
 
 /**
  * CACHING static files
@@ -9,10 +11,56 @@ importScripts("precache-manifest.98676f90810c92abe2f2c23362d9e80a.js", "https://
  */
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
 
+
+// fonts etc should be cached and fetched from cache first, then validated from the network
+workbox.routing.registerRoute(
+  /.*(?:fonts\.googleapis|gstatic)\.com.*$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'google-fonts'
+  })
+)
+// our own static assets
+workbox.routing.registerRoute(
+  new RegExp('/static/'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'static-files'
+  })
+)
+// third-party images (avatars)
+// see https://developers.google.com/web/tools/workbox/modules/workbox-cacheable-response
+workbox.routing.registerRoute(
+  new RegExp('.*(?:.jpg|.png)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxEntries: 20,
+      }),
+    ]
+  })
+)
+
+
 /**
  * dynamic caching
  */
-// workbox.routing.registerRoute('end point url', workbox.strategies.networkFirst({
-//   cacheName: 'cache-name'
-// }));
+workbox.routing.registerRoute(
+  '/home',
+  workbox.strategies.networkFirst({
+    cacheName: 'semi-static'
+  })
+)
 
+
+/**
+ * API GET requests cached with networkFirst
+ */
+workbox.routing.registerRoute(
+  new RegExp('/api/*'),
+  workbox.strategies.networkFirst({
+    cacheName: 'api-get-requests'
+  })
+)
--- resources/assets/js/sw.js
+++ resources/assets/js/sw.js
@@ -1,3 +1,5 @@
+/*global workbox*/
+/*eslint no-undef: "error"*/
 
 /**
  * CACHING static files
@@ -7,9 +9,56 @@
  */
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
 
+
+// fonts etc should be cached and fetched from cache first, then validated from the network
+workbox.routing.registerRoute(
+  /.*(?:fonts\.googleapis|gstatic)\.com.*$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'google-fonts'
+  })
+)
+// our own static assets
+workbox.routing.registerRoute(
+  new RegExp('/static/'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'static-files'
+  })
+)
+// third-party images (avatars)
+// see https://developers.google.com/web/tools/workbox/modules/workbox-cacheable-response
+workbox.routing.registerRoute(
+  new RegExp('.*(?:.jpg|.png)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxEntries: 20,
+      }),
+    ]
+  })
+)
+
+
 /**
  * dynamic caching
  */
-// workbox.routing.registerRoute('end point url', workbox.strategies.networkFirst({
-//   cacheName: 'cache-name'
-// }));
+workbox.routing.registerRoute(
+  '/home',
+  workbox.strategies.networkFirst({
+    cacheName: 'semi-static'
+  })
+)
+
+
+/**
+ * API GET requests cached with networkFirst
+ */
+workbox.routing.registerRoute(
+  new RegExp('/api/*'),
+  workbox.strategies.networkFirst({
+    cacheName: 'api-get-requests'
+  })
+)
